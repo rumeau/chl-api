6e91eb4ea560c31c2fba2e27f6e161ad7526e16a
Jean Rumeau <rumeau@gmail.com>
2015-02-02 18:21:20 -0300

    Corrige typo en nombre de directorio

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends HttpKernel {
 		'auth' => 'App\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
-		'allowOrigin' => 'App\Http\MiddleWare\AllowOrigin',
+		'allowOrigin' => 'App\Http\Middleware\AllowOrigin',
 	];
 
 }
