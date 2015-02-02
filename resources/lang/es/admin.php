7d8869cbbeb98cf11ebf5564c6f2d056b916ba45
Jean Rumeau <rumeau@gmail.com>
2015-02-02 18:07:43 -0300

    Correccion de traduccion

--- resources/lang/es/admin.php
+++ resources/lang/es/admin.php
@@ -26,6 +26,6 @@ return [
     'service_not_found' => 'No se ha encontrado el servicio solicitado',
     'service_edit_success' => 'El servicio ha sido modificado',
     'delete' => 'Eliminar',
-    'delete_services' => 'Eliminar Servicio',
+    'delete_service' => 'Eliminar Servicio',
     'delete_service_agree' => '<p class="text-center">Está seguro que desea eliminar el servicio <strong>:service</strong> del sistema. Los cambios no pueden deshacerse.</p>',
 ];
